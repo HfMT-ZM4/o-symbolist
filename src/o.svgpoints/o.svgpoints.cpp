7685b2481feef32cf9658f345001528c974da5fa
ramagottfried <rama.gottfried@gmail.com>
2019-08-30 18:55:22 +0200

    preparing external dev

---
+++ src/o.svgpoints/o.svgpoints.cpp
@@ -0,0 +1,9 @@
+//
+//  o.cpp
+//  o.svgpoints
+//
+//  Created by Rama Gottfried on 30.08.19.
+//  Copyright © 2019 hfmt-hamburg. All rights reserved.
+//
+
+#include "o.hpp"
---
+++ src/o.svgpoints/o.svgpoints.hpp
@@ -0,0 +1,14 @@
+//
+//  o.hpp
+//  o.svgpoints
+//
+//  Created by Rama Gottfried on 30.08.19.
+//  Copyright © 2019 hfmt-hamburg. All rights reserved.
+//
+
+#ifndef o_hpp
+#define o_hpp
+
+#include <stdio.h>
+
+#endif /* o_hpp */
